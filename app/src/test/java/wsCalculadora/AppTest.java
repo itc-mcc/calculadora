bbe55153b4271b97df8e079b8df81018c766bb3c
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2021-02-28 19:37:52 -0700

    Agregando suma...

--- app/src/test/java/wsCalculadora/AppTest.java
+++ app/src/test/java/wsCalculadora/AppTest.java
@@ -6,9 +6,21 @@ package wsCalculadora;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+
 class AppTest {
+    static Calculadora c;
+
+    @BeforeAll public static void setUp() {
+        c = new Calculadora();
+    }
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void testSuma() {
+        assertTrue(c.suma(2.0,3.0)==5.0);
+    }
+
 }
