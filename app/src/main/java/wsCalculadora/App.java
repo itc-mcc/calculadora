9c56538c7286e510411ee75ddc0dd8f1320a3262
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2021-02-28 19:53:26 -0700

    Agregando la etiqueta 1.0 en la UI...

--- app/src/main/java/wsCalculadora/App.java
+++ app/src/main/java/wsCalculadora/App.java
@@ -8,8 +8,8 @@ public class App {
     public static void main(String[] args) {
         Calculadora c = new Calculadora();
         
-        System.out.println("CALCULADORA");
-        System.out.println("===========");
+        System.out.println("CALCULADORA (v1.0)");
+        System.out.println("=================");
         System.out.println("2+3="+c.suma(2, 3));
     }
 }
